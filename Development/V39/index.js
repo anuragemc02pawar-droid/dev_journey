4ee91969eb7ee4889fde4fe83a4e2d4a79afae68
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-10 21:28:09 +0530

    Dev V39 completed

---
+++ Development/V39/index.js
@@ -0,0 +1,29 @@
+console.log("This is about Conditionals")
+
+let age=1;
+let grace=2
+// age +=grace
+
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age % grace)
+console.log(age ** grace)
+if(age>=18){
+    console.log("You can drive")
+}
+else if(age==0){
+    console.log("Are you kidding me?")
+}
+else if(age==1){
+    console.log("Are you again kidding?")
+}
+else{
+    console.log("You can not drive")
+}
+
+a=6;
+b=8;
+let c=a<b?(a+b):(a-b);
+console.log(c)
