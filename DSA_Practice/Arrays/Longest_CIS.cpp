2f536de33d38cf338ff94d8d819cdad9de59c1d9
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-10 23:52:48 +0530

    L674 solved

---
+++ DSA_Practice/Arrays/Longest_CIS.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        int cnt=1;int ans=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                cnt++;
+            }else{
+                cnt=1;
+            }
+            ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};
