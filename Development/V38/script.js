ec0f590fa1c4dcc6bc31cd46ce20c5c674a2e4aa
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-10 21:27:41 +0530

    Dev V38 completed

---
+++ Development/V38/script.js
@@ -0,0 +1,40 @@
+console.log("Hey this is about JavaScript")
+
+let a=5;
+let b=7;
+let c="Anurag"
+let _a="lol"
+let d=7;
+// let 7a=18; not allowed
+console.log(a+b-5)
+console.log(typeof a, typeof b, typeof c)
+
+const a1=9;
+// a1=9+9;
+// can't do this
+console.log(a1)
+{
+    let d=0
+    console.log(d)
+}
+console.log(d)
+
+let x="Anurag bhai";
+let y=21;
+let z=7.15;
+const m=true;
+let n=undefined;
+let o=null;
+console.log(x, y, z, m, n, o)
+console.log(typeof x,typeof y,typeof z,typeof m,typeof n,typeof o)
+
+let p={
+    "name": "sum",
+    "job code": 1007,
+    "is_handsome": true
+}
+console.log(p)
+p.salary ="15 lakhs"
+console.log(p)
+p.salary="20 lakhs"
+console.log(p)
