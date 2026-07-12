c26705b3179089a56762c6e92274fbff9f2ebdbb
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-12 11:49:28 +0530

    Dev V44

---
+++ Development/V44/index.js
@@ -0,0 +1,51 @@
+let arr = [1, 2, 4, 7, 8]
+arr[0]=7
+console.log(arr)
+console.log(arr.length)
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+let a=[1, 2, 3, 4, 5, 6]
+ a.pop()
+ a.push(7)
+ a.push("anurag")
+ a.shift()
+ a.unshift("Bro")
+ delete a[6]
+ console.log(a[6])
+ console.log(a)
+
+ let a1=[1,2,3]
+ let a2=[4,5,6]
+ let a3=[7,8,9]
+
+ console.log(a1.concat(a2,a3))
+ let numbers=[1,2,3,4,5]
+ console.log(numbers.splice(2,3))
+ console.log(numbers)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
