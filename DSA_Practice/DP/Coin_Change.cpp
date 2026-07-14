6a43a1b0185b16d11dda4f4cb3836bd870aac767
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-14 18:13:41 +0530

    L322 solved

--- DSA_Practice/DP/Coin_Change.cpp
+++ DSA_Practice/DP/Coin_Change.cpp
@@ -1,30 +1,37 @@
 class Solution {
-  public:
-    int subSum(vector<int>& coins, int sum, int n, vector<vector<int>>& dp){
+public:
+    int coinSub(vector<int>& coins, int amount, int n, vector<vector<int>>& dp){
         for(int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
+            for(int j=0;j<amount+1;j++){
                 if(i==0){
-                    dp[0][j]=0;
+                    dp[0][j]=INT_MAX-1;
                 }
                 if(j==0){
-                    dp[i][j]=1;
+                    dp[i][0]=0;
+                }
+                if(i==1){
+                    if((j%coins[0])==0){
+                        dp[1][j]=j/coins[0];
+                    }else{
+                        dp[1][j]=INT_MAX-1;
+                    }
                 }
             }
         }
         for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
+            for(int j=1;j<amount+1;j++){
                 if(j>=coins[i-1]){
-                    dp[i][j]=dp[i][j-coins[i-1]] +dp[i-1][j];
+                    dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
                 }else{
                     dp[i][j]=dp[i-1][j];
                 }
             }
         }
-        return dp[n][sum];
+        return dp[n][amount] == INT_MAX-1 ? -1 : dp[n][amount];
     }
-    int count(vector<int>& coins, int sum) {
+    int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(sum+1,0));
-        return subSum(coins,sum,n,dp);
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        return coinSub(coins,amount,n,dp);
     }
 };
