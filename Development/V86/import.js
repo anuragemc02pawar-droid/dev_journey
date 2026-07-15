e58caabd8fe9c3a252f33be81655cde8e1467144
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-15 21:16:54 +0530

    Dev V86 completed

---
+++ Development/V86/import.js
@@ -0,0 +1,8 @@
+// import{a,b,e} from "./mymodule.js"
+// console.log(a,b,e)
+
+import obj from "./mymodule.js"
+console.log(obj)
+
+import anything from "./mymodule.js"
+console.log(anything)       //default export can be imported with any name
