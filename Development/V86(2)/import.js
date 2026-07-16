8100f85457dea23721730506bce5614bf1e0db24
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-16 20:53:17 +0530

    Dev V86(2) completed

---
+++ Development/V86(2)/import.js
@@ -0,0 +1,4 @@
+const a = require("./mymodule2.js")
+
+
+console.log(a, __dirname, __filename)
---
+++ Development/V86(2)/mymodule2.js
@@ -0,0 +1,9 @@
+module.exports={
+    a:1,
+    b:2,
+    x:9,
+    m:17
+}
+
+// let c=56
+// module.exports = c
