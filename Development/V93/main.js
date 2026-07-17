5400f9e01280ee37f364690b79c34a42c9341ed2
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-17 22:50:29 +0530

    Dev V93 completed

---
+++ Development/V93/main.js
@@ -0,0 +1,21 @@
+import fs, { readdir } from "fs/promises"
+import fsn, { mkdir } from "fs"
+import path from "path"
+
+const basePath ="D:\\Development\\V93"
+
+let files = await readdir(basePath)
+
+for (const item of files) {
+    console.log("running for...", item)
+    let ext = item.split(".")[item.split(".").length-1]
+    if(ext!="js" && ext!="json" && item.split(".").length>1){
+    if(fsn.existsSync(path.join(basePath,ext))){
+        //move the file to this directory only if its not a js or json file
+        fs.rename(path.join(basePath,item),path.join(basePath,ext,item))
+    }else{
+        fs.mkdir(ext)
+        fs.rename(path.join(basePath,item),path.join(basePath,ext,item))
+        }
+    }
+}
