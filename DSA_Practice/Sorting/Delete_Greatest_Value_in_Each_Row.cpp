ad786201f176a613b4f6b9b4fc161e38e43b0b13
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-07-28 14:16:57 +0530

    L2500 solved

---
+++ DSA_Practice/Sorting/Delete_Greatest_Value_in_Each_Row.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+
+        for(int i=0;i<m;i++){
+            sort(grid[i].begin(),grid[i].end());
+        }
+        int ans=0;
+
+        for(int j=0;j<n;j++){
+            int mx=0;
+            for(int i=0;i<m;i++){
+                mx=max(mx,grid[i][j]);
+            }
+            ans+=mx;
+        }
+       return ans;
+
+    }
+};
