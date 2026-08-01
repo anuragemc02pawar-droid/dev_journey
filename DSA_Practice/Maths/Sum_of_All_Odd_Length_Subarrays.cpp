5beebd4f925852e2950b831a6d39d96ee1a965ee
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-08-01 10:32:09 +0530

    L1588 solved

---
+++ DSA_Practice/Maths/Sum_of_All_Odd_Length_Subarrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int n=arr.size();
+        int sum=0;
+        
+        for(int len=1;len<=n;len+=2){
+            for(int i=0;i+len<=n;i++){
+                for(int j=i;j<i+len;j++){
+                    sum+=arr[j];
+                }
+            }
+        }
+        
+        return sum;
+    }
+};
