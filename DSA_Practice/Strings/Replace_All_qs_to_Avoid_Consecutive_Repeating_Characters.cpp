42961b9fcc4ce2bddf4efdb9202d8b73e1ce0b33
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-08-03 23:58:12 +0530

    L1576 solved

---
+++ DSA_Practice/Strings/Replace_All_qs_to_Avoid_Consecutive_Repeating_Characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string modifyString(string s) {
+        int n=s.length();
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='?'){
+                for(char ch='a';ch<'z';ch++){
+                    if((i>0 && s[i-1]==ch) || (i<n-1 && s[i+1]==ch)){
+                        continue;
+                    }
+                    s[i]=ch;
+                }
+            }
+        }
+        return s;
+
+    }
+};
