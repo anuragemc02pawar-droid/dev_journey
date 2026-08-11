3b8707a93644a43922ea671162ceb5de6380f463
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-08-11 21:56:05 +0530

    L560 solved

---
+++ DSA_Practice/Hashing/Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0]=1;
+
+        int ans=0;
+        int sum=0;
+
+        for(auto x:nums){
+            sum+=x;
+
+            if(mp.count(sum-k)){
+                ans+=mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
