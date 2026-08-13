b92299fca0f07699e3dfad3067df4bc7744abec8
Anurag Pawar <anuragemc.02pawar@gmail.com>
2026-08-13 23:22:39 +0530

    L1346 solved

---
+++ DSA_Practice/Hashing/Check_If_N_and_Its_Double_Exist.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> st;
+
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]%2==0 && st.count(arr[i]/2)){
+                return true;
+            }
+            if(st.count(arr[i]*2)){
+                return true;
+            }
+            st.insert(arr[i]);
+        }
+        return false;
+    }
+};
